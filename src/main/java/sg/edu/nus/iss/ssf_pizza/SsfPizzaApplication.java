10c830cb1de789a1e88f562d49d201fc93b00e6f
russell258 <duckie258@gmail.com>
2023-06-06 17:44:45 +0800

    setup

---
+++ src/main/java/sg/edu/nus/iss/ssf_pizza/SsfPizzaApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf_pizza/SsfPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
